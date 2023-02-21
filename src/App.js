7d19278753de75d81856c87247b25abd0b5a0efb
Francis Maina <sebastianfrancix@gmail.com>
2023-02-21 17:42:32 +0300

    added routes for homepage and specific exercise page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
 import "./App.css";
 
 function App() {
-  return <h1>App Component</h1>;
+  return (
+    <Box width="400px">
+      <Navbar />
+
+      <Routes>
+        {/* Route / to Homepage */}
+        <Route path="/" element={<Home />} />
+        {/* Route to a specific exercise based on their id */}
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
