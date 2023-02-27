a66019df2d67763cc6ed2127127123b5c0ba816d
Francis Maina <sebastianfrancix@gmail.com>
2023-02-28 02:44:31 +0300

    added boilerplate code for components and pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
   return (
