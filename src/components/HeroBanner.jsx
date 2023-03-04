23158008985b140c958054e2e58d799228b92792
Francis Maina <sebastianfrancix@gmail.com>
2023-03-04 12:50:09 +0300

    added HeroBanner content, Explore Exercises button and banner image

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png"
 
 const HeroBanner = () => {
   return (
     <Box
+      // stylings
       sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
       position="relative"
       p="20px"
@@ -11,6 +14,25 @@ const HeroBanner = () => {
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
+
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+
+      <Typography fontSize="22px" lineHeight="35px" mb={3}>
+        Check out the most effective exercises
+      </Typography>
+
+      {/* Explore Exercise Button */}
+      <Button variant="contained" color="error" href="#exercises">
+        Explore Exercises
+      </Button>
+
+      {/* Banner Image */}
+      <img src={HeroBannerImage} alt="banner image" className="hero-banner-img" />
     </Box>
   );
 };
