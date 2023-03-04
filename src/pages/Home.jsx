7e4426036adfdc481e74313e06213123e6d08d28
Francis Maina <sebastianfrancix@gmail.com>
2023-03-04 12:18:32 +0300

    added styling to our MUI Box component inside HeroBanner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
+
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
