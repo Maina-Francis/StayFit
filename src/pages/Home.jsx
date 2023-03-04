86f585dd5e5deae2406beafaa7bb3ef5c19db060
Francis Maina <sebastianfrancix@gmail.com>
2023-03-04 12:00:11 +0300

    added HeroBanner component boiler plate and rendered it in the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;
