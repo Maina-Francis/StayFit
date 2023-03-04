80d3a0a22c937a4e541e561cc4290b7997fb5e89
Francis Maina <sebastianfrancix@gmail.com>
2023-03-05 02:20:25 +0300

    created the HorizontalScrollbar component to render the categories

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box } from "@mui/material";
 
-const HorizontalScrollbar = () => {
-  return <div>HorizontalScrollbar</div>;
+const HorizontalScrollbar = ({ data }) => {
+  //   console.log(data);
+  return (
+    <div>
+      {/* map over our list of categories data */}
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          margin="0 40px"
+        >
+          {item}
+        </Box>
+      ))}
+    </div>
+  );
 };
 
 export default HorizontalScrollbar;
