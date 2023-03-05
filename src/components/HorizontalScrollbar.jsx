492b8f7a851691267421c0011ccf5bb9df0e508c
Francis Maina <sebastianfrancix@gmail.com>
2023-03-05 03:45:11 +0300

    added left and right arrow icons to enable horizontal scrolling of categories icons

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useContext } from "react";
+import { Box, Typography } from "@mui/material";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 
 import BodyPart from "./BodyPart";
 
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+
+// Left arrow for horizontal scrolling
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+// Right arrow for horizontal scrolling
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   //   console.log(data);
   return (
-    <ScrollMenu>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {/* map over our list of categories data */}
       {data.map((item) => (
         <Box
