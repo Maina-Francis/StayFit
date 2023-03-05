a20152c3c308f7fab9034535cf12d04c871ee791
Francis Maina <sebastianfrancix@gmail.com>
2023-03-05 15:28:44 +0300

    added the ExerciseCard component to display the exercises gif, name, target muscle and bodypart

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h4" mb="46px">
+        Showing Results
+      </Typography>
+
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {/* map over all our exercises */}
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
