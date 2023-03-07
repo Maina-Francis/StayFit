3ec6d381f5ac7cb156de30c1443674f08d1b3d5c
Francis Maina <sebastianfrancix@gmail.com>
2023-03-07 03:45:24 +0300

    fetched exercise videos from the youtube search and download api and passed it to the ExerciseVideos component as prop

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import SimilarExercises from "../components/SimilarExercises";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,12 +17,21 @@ const ExerciseDetail = () => {
       const youtubeSearchUrl =
         "https://youtube-search-and-download.p.rapidapi.com";
 
+      // api call to exerciseDB api
       const exerciseDetailData = await fetchData(
         `${exerciseDbUrl}/exercises/exercise/${id}`,
         exerciseOptions
       );
 
       setExerciseDetail(exerciseDetailData);
+
+      // api call to Youtube search and download api
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+
+      setExerciseVideos(exerciseVideosData);
     };
 
     fetchExercisesData();
@@ -30,7 +40,10 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
       <SimilarExercises />
     </Box>
   );
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,11 +6,22 @@ export const fetchData = async (url, options) => {
   return data;
 };
 
-// from Rapid API
+// from Rapid API ExerciseDB api
 export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+// from Rapid API Youtube Search and Download api
+
+export const youtubeOptions = {
+  method: "GET",
+
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
